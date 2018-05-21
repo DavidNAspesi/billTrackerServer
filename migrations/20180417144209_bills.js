2c7dedb14730216fe41049a508cccd3559e456cd
David Aspesi <davidnaspesi@gmail.com>
2018-05-21 09:43:18 -0600

    about to deploy server

--- migrations/20180417144209_bills.js
+++ migrations/20180417144209_bills.js
@@ -1,6 +1,7 @@
 
 exports.up = function(knex, Promise) {
     return knex.schema.createTable("bills", bills => {
+        bills.increments()
         bills.integer("user_id")
         bills.string("type")
         bills.float("amount")
--- seeds/02-bills.js
+++ seeds/02-bills.js
@@ -3,21 +3,25 @@ exports.seed = function(knex, Promise) {
   return knex("bills").del()
     .then(function () {
       return knex("bills").insert([{
+        id: 1,
         user_id: 1, 
         type: "rent",
         amount: 1000.00,
         due_date: "2018-05-01"
       },{
+        id: 2,
         user_id: 2, 
         type: "rent",
         amount: 1000.00,
         due_date: "2018-05-01"
       },{
+        id: 3,
         user_id: 2, 
         type: "electricity",
         amount: 25.00,
         due_date: "2018-04-25"
       },{
+        id: 4,
         user_id: 3, 
         type: "electricity",
         amount: 45.00,
